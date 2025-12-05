878f0816414131b8041cb1e211354f82c4b635fa
Joachim Meyer <jmeyer@boisestate.edu>
2025-12-05 10:19:47 -0600

    Dash - Improve startup script
    Add PID and chdir to gunicorn start process. Also show the tail of the error
    log after starting the server

--- dash/start_dash_app.sh
+++ dash/start_dash_app.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
-cd /nvm9/data/dash_app
+set -e
 
-micromamba run -n dash\
+APP_HOME="/nvm9/data/dash_app"
+ERROR_LOG="${APP_HOME}/logs/dash_error.log"
+
+micromamba run -n dash \
   gunicorn \
     --workers=1 --threads=8 \
-    --error-logfile logs/dash_error.log \
+    --error-logfile ${ERROR_LOG} \
     --capture-output \
     --daemon \
+    --pid "${APP_HOME}/gunicorn.pid" \
+    --chdir ${APP_HOME} \
     --bind 0.0.0.0:8050 Snow17_vs_iSnobal:server &
+
+sleep 3
+
+tail ${ERROR_LOG}
+
