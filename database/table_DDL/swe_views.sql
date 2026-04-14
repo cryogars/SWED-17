a89196508cca4aff13e925e00aab530e43d7bf92
Joachim Meyer <jmeyer@boisestate.edu>
2026-04-14 18:51:11 +0000

    DB - Views - Update zonal_SWE
    Revisit the SQL and simplify with the help of Gemini. This now shows all
    datasets with their data or 0 in case of a data gap.

--- database/table_DDL/swe_views.sql
+++ database/table_DDL/swe_views.sql
@@ -2,48 +2,22 @@
 
 DROP VIEW IF EXISTS public.zonal_swe;
 CREATE OR REPLACE VIEW public.zonal_swe
-AS WITH swe_one AS (
-         SELECT
-            COALESCE(isz.cbrfc_zone_id, ssz.cbrfc_zone_id) AS cbrfc_zone_id,
-            COALESCE(isz.datetime, ssz.datetime) AS date,
-            isz.value AS isnobal_swe,
-            ssz.value AS snodas_swe
-           FROM isnobal_zonal_swe isz
-             FULL JOIN snodas_zonal_swe ssz ON isz.cbrfc_zone_id = ssz.cbrfc_zone_id AND isz.datetime = ssz.datetime
-          WHERE isz.isnobal_version_id = 2 AND EXTRACT(hour FROM isz.datetime) = 0::numeric
-    ),
-    swe_two AS (
-         SELECT
-            COALESCE(sv.cbrfc_zone_id, csz.cbrfc_zone_id) AS cbrfc_zone_id,
-            COALESCE(sv.date, csz.datetime) AS date,
-            sv.isnobal_swe,
-            sv.snodas_swe,
-            csz.value AS cu_boulder_swe
-           FROM swe_one sv
-             FULL JOIN cu_boulder_zonal_swe csz
-                ON sv.cbrfc_zone_id = csz.cbrfc_zone_id AND sv.date = csz.datetime
-    ),
-    swe_three AS (
-         SELECT
-            COALESCE(sv.cbrfc_zone_id, asz.cbrfc_zone_id) AS cbrfc_zone_id,
-            COALESCE(sv.date, asz.datetime) AS date,
-            sv.isnobal_swe,
-            sv.snodas_swe,
-            sv.cu_boulder_swe,
-            asz.value AS aso_swe
-           FROM swe_two sv
-             FULL JOIN aso_zonal_swe asz
-                ON sv.cbrfc_zone_id = asz.cbrfc_zone_id AND sv.date = asz.datetime
-    )
- SELECT
-    sv.date,
-    cz.zone AS zone_name,
-    sv.isnobal_swe,
-    sv.snodas_swe,
-    uzs.value AS ua_swe,
-    sv.cu_boulder_swe,
-    sv.aso_swe,
-    sv.cbrfc_zone_id
-   FROM swe_three sv
-     FULL JOIN ua_zonal_swe uzs ON sv.cbrfc_zone_id = uzs.cbrfc_zone_id AND sv.date = uzs.datetime
-     LEFT JOIN cbrfc_zones cz ON COALESCE(sv.cbrfc_zone_id, uzs.cbrfc_zone_id) = cz.gid;
+AS SELECT
+    datetime AS date,
+    COALESCE(isz.value, 0) AS isnobal_swe,
+    COALESCE(ssz.value, 0) AS snodas_swe,
+    COALESCE(uzs.value, 0) AS ua_swe,
+    COALESCE(csz.value, 0) AS cu_boulder_swe,
+    asz.value AS aso_swe,
+    cbrfc_zone_id
+FROM snodas_zonal_swe ssz
+FULL JOIN (
+    SELECT cbrfc_zone_id, datetime, value
+    FROM isnobal_zonal_swe
+    WHERE isnobal_version_id = 2
+      AND EXTRACT(HOUR FROM datetime AT TIME ZONE 'UTC') = 0
+) isz USING (cbrfc_zone_id, datetime)
+FULL JOIN ua_zonal_swe         uzs USING (cbrfc_zone_id, datetime)
+FULL JOIN cu_boulder_zonal_swe csz USING (cbrfc_zone_id, datetime)
+FULL JOIN aso_zonal_swe        asz USING (cbrfc_zone_id, datetime)
+LEFT JOIN cbrfc_zones          cz  ON cbrfc_zone_id = cz.gid;
