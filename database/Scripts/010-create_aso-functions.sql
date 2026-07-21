b26bfb9975315f47c5f9df02bb449fe4659e00d7
Joachim Meyer <jmeyer@boisestate.edu>
2026-07-21 17:14:08 +0000

    DB - Functions - Update ASO function to use view and simplify

--- database/Scripts/010-create_aso-functions.sql
+++ database/Scripts/010-create_aso-functions.sql
@@ -23,26 +23,18 @@ CREATE OR REPLACE FUNCTION public.aso_swe_at_station(target_station_name text)
 RETURNS TABLE(swe_date date, raw_pixel_value double precision) AS $$
 BEGIN
     RETURN QUERY EXECUTE format('
-        WITH transformed_sites AS (
-            SELECT
-                ST_Transform(ss.geometry, ST_SRID(r.rast)) AS target_geom
-            FROM
-                snotel_sites AS ss,
-                aso_swe_13n AS r
-            WHERE
-                ss.station_name = %L
-            LIMIT 1
-        )
         SELECT
-            swe_raster.swe_date,
+            r.swe_date,
             ST_Value(
-                swe_raster.rast,
-                ts.target_geom
-            ) AS raw_pixel_value
+                r.rast,
+                ST_Transform(ss.geometry, ST_SRID(r.rast))
+            )
         FROM
-            aso_swe_13n AS swe_raster
-        JOIN
-            transformed_sites AS ts ON  swe_raster.rast && ts.target_geom;
+            snotel_sites AS ss,
+            aso_swe AS r
+        WHERE
+            ss.station_name = %L
+            AND r.rast && ST_Transform(ss.geometry, ST_SRID(r.rast));
     ',
         target_station_name
     );
