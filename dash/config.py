2a95243c7f12eb8316d9c14970debf66fd08b2a3
Joachim Meyer <jmeyer@boisestate.edu>
2026-08-04 18:15:19 +0000

    Dash - Remove unused YEAR configuration value

--- dash/config.py
+++ dash/config.py
@@ -1,6 +1,4 @@
 # First date with availalbe data
 START_DATE = "2020-10-01"
-# Last water year with available data
-END_YEAR = 2025
 # Current list of products
 DATASETS = ["Snow-17", "iSnobal", "SNODAS", "UArizona", "CU Boulder", "ASO"]
